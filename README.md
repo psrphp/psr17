5a3e0acae575b38042393f502f03b25fd0059136
John Doe <johndoe@example.com>
2023-05-08 11:26:02 +0800

    1

---
+++ README.md
@@ -0,0 +1,25 @@
+# psr17
+
+php psr17
+
+## Installation
+
+``` cmd
+composer require psrphp/psr17
+```
+
+## Usage
+
+``` php
+$factory = new \PsrPHP\Psr17\Factory;
+
+$request = $factory->createRequest(string $method, $uri);
+$response = $factory->createResponse(int $code = 200, string $reasonPhrase = '');
+$response = $factory->createGeneralResponse(int $code = 200, array $headers = [], string $body = null);
+$server_request = $factory->createServerRequest(string $method, $uri, array $serverParams = []);
+$stream = $factory->createStream(string $content = '');
+$stream = $factory->createStreamFromFile(string $file, string $mode = 'r');
+$stream = $factory->createStreamFromResource($resource);
+$upload_file = $factory->createUploadedFile(StreamInterface $stream,int $size = null,int $error = \UPLOAD_ERR_OK,string $clientFilename = null,string $clientMediaType = null);
+$uri = $factory->createUri(string $uri = '');
+```
---
+++ src/Factory.php
@@ -0,0 +1,107 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PsrPHP\Psr17;
+
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\ServerRequest;
+use GuzzleHttp\Psr7\UploadedFile;
+use GuzzleHttp\Psr7\Uri;
+use GuzzleHttp\Psr7\Utils;
+use InvalidArgumentException;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Psr\Http\Message\UriFactoryInterface;
+use Psr\Http\Message\UriInterface;
+
+class Factory implements
+    RequestFactoryInterface,
+    ResponseFactoryInterface,
+    ServerRequestFactoryInterface,
+    StreamFactoryInterface,
+    UploadedFileFactoryInterface,
+    UriFactoryInterface
+{
+    public function createRequest(string $method, $uri): RequestInterface
+    {
+        return new Request($method, $uri);
+    }
+
+    public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface
+    {
+        return new Response($code, [], null, '1.1', $reasonPhrase);
+    }
+
+    public function createGeneralResponse(int $code = 200, array $headers = [], string $body = null): ResponseInterface
+    {
+        $response = new Response($code, $headers);
+        $response->getBody()->write($body);
+        return $response;
+    }
+
+    public function createServerRequest(string $method, $uri, array $serverParams = []): ServerRequestInterface
+    {
+        if (empty($method)) {
+            if (!empty($serverParams['REQUEST_METHOD'])) {
+                $method = $serverParams['REQUEST_METHOD'];
+            } else {
+                throw new InvalidArgumentException('Cannot determine HTTP method');
+            }
+        }
+        return new ServerRequest($method, $uri, [], null, '1.1', $serverParams);
+    }
+
+    public function createServerRequestFromGlobals(): ServerRequestInterface
+    {
+        return ServerRequest::fromGlobals();
+    }
+
+    public function createStream(string $content = ''): StreamInterface
+    {
+        return Utils::streamFor($content);
+    }
+
+    public function createStreamFromFile(string $file, string $mode = 'r'): StreamInterface
+    {
+        $resource = Utils::tryFopen($file, $mode);
+        return Utils::streamFor($resource);
+    }
+
+    public function createStreamFromResource($resource): StreamInterface
+    {
+        return Utils::streamFor($resource);
+    }
+
+    public function createUploadedFile(
+        StreamInterface $stream,
+        int $size = null,
+        int $error = \UPLOAD_ERR_OK,
+        string $clientFilename = null,
+        string $clientMediaType = null
+    ): UploadedFileInterface {
+        if ($size === null) {
+            $size = $stream->getSize();
+        }
+        return new UploadedFile($stream, $size, $error, $clientFilename, $clientMediaType);
+    }
+
+    public function createUri(string $uri = ''): UriInterface
+    {
+        return new Uri($uri);
+    }
+
+    public function createUriFromGlobals(): UriInterface
+    {
+        return ServerRequest::getUriFromGlobals();
+    }
+}
